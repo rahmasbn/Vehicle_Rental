f1409523fd7f23ea8e41e6f485475d0be49d4a25
Rahmasbn <rahmasyabani37@gmail.com>
2022-01-25 01:17:33 +0700

    adding route

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -40,9 +40,9 @@ const getTransactionByVehicleType = (order, typeId) => {
 
 // const getAllTransaction = (query) => {
 //   return new Promise((resolve, reject) => {
-//     let sqlQuery = `SELECT t.id, u.name AS "name", u.address AS "address", v.name AS "vehicle", v.type_id, v.city_id, 
-//     v.price AS "price", t.quantity AS "qty", t.total_payment AS "total payment", t.start_date AS "start date", 
-//     t.return_date AS "return date", t.status AS "status", t.rating AS "rating" FROM transaction t 
+//     let sqlQuery = `SELECT t.id, u.name AS "name", u.address AS "address", v.name AS "vehicle", v.type_id, v.city_id,
+//     v.price AS "price", t.quantity AS "qty", t.total_payment AS "total payment", t.start_date AS "start date",
+//     t.return_date AS "return date", t.status AS "status", t.rating AS "rating" FROM transaction t
 //     JOIN users u ON t.user_id = u.id JOIN vehicles v ON t.vehicle_id = v.id`;
 
 //     const prepStatement = [];
@@ -57,7 +57,7 @@ const getTransactionByVehicleType = (order, typeId) => {
 //       let limit = parseInt(query.limit);
 //       let offset = '';
 //       const count = result[0].count;
-      
+
 //       if (!query.page && !query.limit) {
 //         page = 1; limit = 3; offset = 0;
 //         sqlQuery += " LIMIT ? OFFSET ?";
@@ -72,7 +72,7 @@ const getTransactionByVehicleType = (order, typeId) => {
 //         next: page == Math.ceil(count / limit) ? null : `/vehicles?page=${page + 1}&limit=${limit}`+data,
 //         prev: page == 1 ? null : `/vehicles?page=${page - 1}&limit=${limit}`+data,
 //       };
-    
+
 //     db.query(sqlQuery, prepStatement, (err, result) => {
 //       if (err) return reject({ status: 500, err });
 //       resolve({ status: 200, result: { meta, data: result } });
@@ -81,28 +81,30 @@ const getTransactionByVehicleType = (order, typeId) => {
 // });
 // };
 
-const getTransaction = (query,id) => {
+const getTransaction = (query, id) => {
   return new Promise((resolve, reject) => {
-    let sqlQuery = `SELECT t.id, u.name AS "name", u.address AS "address", v.name AS "vehicle", v.type_id, v.city_id, 
-    v.price AS "price", t.quantity AS "qty", t.total_payment AS "total payment", t.start_date AS "start date", 
-    t.return_date AS "return date", t.status AS "status", t.rating AS "rating" FROM transaction t 
+    let sqlQuery = `SELECT t.id, u.name AS "name", u.address AS "address", v.name AS "vehicle", v.type_id, v.city_id, v.images, 
+    v.price AS "price", t.quantity AS "qty", t.total_payment, t.start_date,
+    t.return_date, t.status, t.rating FROM transaction t 
     JOIN users u ON t.user_id = u.id JOIN vehicles v ON t.vehicle_id = v.id WHERE t.user_id = ${id}`;
 
     const prepStatement = [];
-    let data = '';
+    let data = "";
 
-    const countQuery = `SELECT COUNT(*) AS "count" FROM transaction`;
+    const countQuery = `SELECT COUNT(*) AS "count" FROM transaction WHERE user_id = ${id}`;
     db.query(countQuery, (err, result) => {
       if (err) return reject({ status: 500, err });
 
       // Paginasi
       let page = parseInt(query.page);
       let limit = parseInt(query.limit);
-      let offset = '';
+      let offset = "";
       const count = result[0].count;
-      
+
       if (!query.page && !query.limit) {
-        page = 1; limit = 3; offset = 0;
+        page = 1;
+        limit = 3;
+        offset = 0;
         sqlQuery += " LIMIT ? OFFSET ?";
         prepStatement.push(limit, offset);
       } else {
@@ -112,16 +114,22 @@ const getTransaction = (query,id) => {
       }
       const meta = {
         count,
-        next: page == Math.ceil(count / limit) ? null : `/transaction?page=${page + 1}&limit=${limit}`+data,
-        prev: page == 1 ? null : `/transaction?page=${page - 1}&limit=${limit}`+data,
+        next:
+          page == Math.ceil(count / limit)
+            ? null
+            : `/transaction?page=${page + 1}&limit=${limit}` + data,
+        prev:
+          page == 1
+            ? null
+            : `/transaction?page=${page - 1}&limit=${limit}` + data,
       };
-    
-    db.query(sqlQuery, prepStatement, (err, result) => {
-      if (err) return reject({ status: 500, err });
-      resolve({ status: 200, result: { meta, data: result } });
+
+      db.query(sqlQuery, prepStatement, (err, result) => {
+        if (err) return reject({ status: 500, err });
+        resolve({ status: 200, result: { meta, data: result } });
+      });
     });
   });
-});
 };
 
 const getDetailTransactionById = (transactionId) => {
@@ -132,7 +140,7 @@ const getDetailTransactionById = (transactionId) => {
     t.status AS "status", t.rating AS "rating" FROM transaction t JOIN users u ON t.user_id = u.id 
     JOIN vehicles v ON t.vehicle_id = v.id JOIN types ON v.type_id = types.id JOIN cities c ON v.city_id = c.id 
     WHERE t.id = ${transactionId}`;
-    
+
     db.query(sqlQuery, (err, result) => {
       if (err) return reject({ status: 500, err });
       if (result.length == 0) return resolve({ status: 404, result });
