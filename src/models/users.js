ac3b71c1c8b3a24ac9e57824d21d08288733f4f7
Rahmasbn <rahmasyabani37@gmail.com>
2022-02-28 17:14:37 +0700

    fixing response

--- src/models/users.js
+++ src/models/users.js
@@ -36,26 +36,26 @@ const updatePassword = (body, id) => {
         // console.log(hashedPassword)
         const checkPassword = await bcrypt.compare(currentPass, hashedPassword);
         // console.log('currentpass: ', currentPass, 'hashPassword: ',hashedPassword)
-        console.log(checkPassword)
-        if(!checkPassword) return reject({status: 401, err});
+        console.log(checkPassword);
+        if (!checkPassword) return reject({ status: 401, err });
 
         // if (checkPassword) {
-          const sqlQuery = `UPDATE users SET password = ? WHERE id = ?`;
-          bcrypt
-            .hash(newPass, 10)
-            .then((hashedPassword) => {
-             const password= hashedPassword;
-              // console.log(password)
-              db.query(sqlQuery, [password, id], (err, result) => {
-                if (err) return reject({ status: 500, err });
-                return resolve({ status: 200, result });
-              });
-            })
-            .catch((err) => {
-              reject({ status: 500, err });
+        const sqlQuery = `UPDATE users SET password = ? WHERE id = ?`;
+        bcrypt
+          .hash(newPass, 10)
+          .then((hashedPassword) => {
+            const password = hashedPassword;
+            // console.log(password)
+            db.query(sqlQuery, [password, id], (err, result) => {
+              if (err) return reject({ status: 500, err });
+              return resolve({ status: 200, result });
             });
+          })
+          .catch((err) => {
+            reject({ status: 500, err });
+          });
         // }
-        // 
+        //
       } catch (err) {
         reject({ status: 500, err });
       }
@@ -76,7 +76,8 @@ const getAllUsers = () => {
 
 const getUserData = (id) => {
   return new Promise((resolve, reject) => {
-    const sqlQuery = `SELECT * FROM users WHERE id = ?`;
+    const sqlQuery = `SELECT u.name, u.email, u.dob, u.gender_id, u.address, u.phone_number, u.image 
+    FROM users u WHERE id = ?`;
     db.query(sqlQuery, id, (err, result) => {
       if (err) return reject({ status: 500, err });
       resolve({ status: 200, result });
